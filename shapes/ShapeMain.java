987e35c04567c4422320340ba5e1b800daf38402
Bappi003 <it24046@mbstu.ac.bd>
2026-06-15 15:28:03 +0600

    Update ShapeMain.java

--- shapes/ShapeMain.java
+++ shapes/ShapeMain.java
@@ -1,18 +1,84 @@
 package shapes;
 
+import java.util.Scanner;
+
 public class ShapeMain {
 
     public static void main(String[] args) {
 
-        Rectangle r =
-                new Rectangle("Red", true, 4, 5);
+        Scanner sc = new Scanner(System.in);
 
-        Circle c =
-                new Circle("Blue", false, 3);
+        int n = sc.nextInt();
 
-        System.out.println(r);
-        System.out.println();
+        Shape[] shapes = new Shape[n];
 
-        System.out.println(c);
+        for (int i = 0; i < n; i++) {
+
+            String type = sc.next();
+            String color = sc.next();
+            boolean filled = sc.nextBoolean();
+
+            if (type.equals("RECTANGLE")) {
+
+                double width = sc.nextDouble();
+                double length = sc.nextDouble();
+
+                shapes[i] = new Rectangle(
+                        color,
+                        filled,
+                        width,
+                        length
+                );
+
+            } else if (type.equals("CIRCLE")) {
+
+                double radius = sc.nextDouble();
+
+                shapes[i] = new Circle(
+                        color,
+                        filled,
+                        radius
+                );
+            }
+        }
+
+        for (Shape s : shapes) {
+            System.out.println(s);
+            System.out.println();
+        }
+
+        System.out.println("--- Downcast Check ---");
+
+        for (Shape s : shapes) {
+
+            if (s instanceof Rectangle) {
+
+                Rectangle r = (Rectangle) s;
+
+                System.out.println(
+                        "Rectangle width=" +
+                        r.getWidth() +
+                        " length=" +
+                        r.getLength()
+                );
+
+            } else if (s instanceof Circle) {
+
+                Circle c = (Circle) s;
+
+                System.out.println(
+                        "Circle radius=" +
+                        c.getRadius()
+                );
+            }
+        }
+
+        /*
+         * ANSWER:
+         * error: Shape is abstract;
+         * cannot be instantiated
+         */
+
+        sc.close();
     }
 }
